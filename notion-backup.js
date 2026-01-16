1c224bddecb0a2448f0a46b29ec7184b25211419
Joyce <>
2026-01-16 15:41:56 +0800

    feat：优化打印

--- notion-backup.js
+++ notion-backup.js
@@ -111,7 +111,7 @@ async function exportFromNotion (format) {
         async () => post('getNotificationLog', { spaceId: `${NOTION_SPACE_ID}`, size: 1, type: 'unread_and_read' })
       );
 
-      // console.warn('数据获取成功', response);
+      console.warn('数据获取成功', response.data, JSON.stringify(response.data));
 
       let { activity } = response.data.recordMap;
 
@@ -171,6 +171,10 @@ async function exportFromNotion (format) {
       } else {
         break;
       }
+    }
+
+    if (!exportURL) {
+      throw new Error('最终未找到正确的链接');
     }
 
 
